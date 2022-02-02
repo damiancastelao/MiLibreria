9473ce5fae2ede1a422e119ad26d603fd2cde70a
Sergio Fernandez Martinez <sfernandezmartinez@danielcastelao.org>
2022-02-02 14:41:23 +0100

    primer cambio

--- src/libreriasv8/Consola.java
+++ src/libreriasv8/Consola.java
@@ -4,6 +4,7 @@ package libreriasv8;
  * Esta clase es un producto
  */
 public class Consola implements InterfaceMetodosComunes {
+
     private String msg;
 
     /**
@@ -21,4 +22,5 @@ public class Consola implements InterfaceMetodosComunes {
     public void visualiza(){
         System.out.println("Sale por la consola " + msg);
     }
+    //CHIMICHANGAS
 }
